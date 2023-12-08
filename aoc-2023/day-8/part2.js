bba245195630ecac8d244accd690e7244c0195a7
Andi Xie <andi.xie@salesforce.com>
2023-12-08 10:45:34 -0800

    day 8

--- aoc-2023/day-8/part2.js
+++ aoc-2023/day-8/part2.js
@@ -12,19 +12,27 @@ import * as fs from 'fs'
 function solution(input) {
     const [path, mapStr] = input.split('\n\n');
 
-    let target = 'ZZZ';
-    let head = makeTree(mapStr);
+    let heads = makeTree(mapStr);
 
     let [count, index] = [0, 0];
-    while(true) {
-        const direction = path[index];
-        head = head[direction];
+
+    let steps = [];
+    while(heads.length) {
         count += 1;
-        if(head.value === target) {
-            return count;
-        }
+        heads = heads.map(head => {
+            let direction = path[index];
+            return head[direction];
+        }).filter(head => {
+            if(head.onZ) {
+                steps.push(count);
+            }
+            return !head.onZ;
+        })
         index = index < path.length - 1 ? index + 1 : 0;
     }
+    // need to find the least common multiple of the steps each ghost takes
+    // as the the path they take is circular
+    return steps.reduce((acc, curr) => lcm(acc, curr),1);
 }
 
 
@@ -32,19 +40,19 @@ function solution(input) {
  * make Tree
  *
  * @param {string} inputMap
- * @returns {TreeNode} head
+ * @returns {TreeNode[]} head
  */
 function makeTree(inputMap) {
     const treeMap = new Map();
-    let head;
+    let heads = new Set();
     for(const line of inputMap.split('\n')) {
         let [nodeValue, childrenVal] = line.replace(/[()\s]/g, '').split('=');
         if(!treeMap.has(nodeValue)) {
             treeMap.set(nodeValue, new TreeNode(nodeValue));
         }
         const curr = treeMap.get(nodeValue);
-        if(nodeValue === 'AAA') {
-            head = curr;
+        if(nodeValue.endsWith('A')) {
+            heads.add(curr);
         }
         const [leftVal, rightVal] = childrenVal.split(',');
         if(!treeMap.has(leftVal)) {
@@ -55,7 +63,7 @@ function makeTree(inputMap) {
         }
         [curr.L, curr.R] = [treeMap.get(leftVal), treeMap.get(rightVal)];
     }
-    return head;
+    return Array.from(heads);
 }
 
 
@@ -63,6 +71,11 @@ class TreeNode {
     constructor(value) {
         this.value = value;
     }
+
+    get onZ() {
+        return this.value.endsWith('Z');
+    }
+
     /**
      * value
      * @type {string}
@@ -82,9 +95,19 @@ class TreeNode {
 }
 
 
+function lcm(a, b) {
+    let [large, small] = [Math.max(a, b), Math.min(a, b)];
+    let res = large;
+    while(res % small !== 0) {
+        res += large
+    }
+    return res;
+}
+
+
 const input = fs.readFileSync('./input.txt', 'utf8');
 
-console.log(`the solution 1 is: ${solution(input.trim())}`);
+console.log(`the solution 2 is: ${solution(input.trim())}`);
 
 export {solution}
 
---
+++ aoc-2023/day-8/part2.test.js
@@ -0,0 +1,16 @@
+import {solution} from "./part2.js"
+
+const input = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
+
+test('it works', () => {
+    expect(solution(input)).toBe(6);
+})
